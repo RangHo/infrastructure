4b41efc004ebd694f4972b0e7b3216e6762f7055
RangHo Lee <hello@rangho.me>
2026-07-10 13:59:20 +0900

    feat(breakroom): add cloudflared

--- machines/breakroom/configuration.nix
+++ machines/breakroom/configuration.nix
@@ -1,4 +1,5 @@
 {
+  config,
   vars,
   ...
 }:
@@ -6,12 +7,21 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/cloudflared.nix
     ../../modules/employee.nix
     ../../modules/impermanence.nix
     ../../modules/partition.nix
   ];
 
-  networking.hostName = "breakroom";
+  age.secrets.cloudflaredCredentialsFile.file = ../../secrets/breakroom-cloudflared-credential-file.age;
+
+  networking = {
+    hostName = "breakroom";
+    cloudflared = {
+      uuid = "31496a40-ea0d-4efc-8d28-d59397710888";
+      credentialsFile = config.age.secrets.cloudflaredCredentialsFile.path;
+    };
+  };
   partitioning.diskName = "/dev/disk/by-id/nvme-GV-256_2280_GD50514111323";
 
   homelab = {
